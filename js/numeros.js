db295d45ab685ab800893ff802deb4365d73c7c9
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-05-08 13:05:29 -0300

    implementado a biblioteca c3.js, feito o gráfico de docentes e servidores

--- js/numeros.js
+++ js/numeros.js
@@ -1,64 +1,35 @@
-
-var docentes;
 var base = 'https://api.fflch.usp.br/pessoa/vinculo/total/ativos/';
 
 window.onload = function() {
-  docentes();
-  servidores();
+    var dataChart = [];
 
-
-var dataset = [
-    {
-        value: 1,
-        name: 'glittering'
-    },
-    {
-        value: 1,
-        name: 'luminous'
-    }
-];
-
-const barChart = new britecharts.bar();
- const chartTooltip = new britecharts.tooltip();
-
-
-barChart
-  //    .width(containerWidth)
-    .height(300)
-  //    .hasPercentage(true)
-    .enableLabels(true)
-    .labelsNumberFormat('.0%')
-    .isAnimated(true)
-    .on('customMouseOver', tooltip.show)
-    .on('customMouseMove', tooltip.update)
-    .on('customMouseOut', tooltip.hide);
-
-barContainer.datum(dataset).call(barChart);
-
-tooltip
-    .numberFormat('.2%');
-
-tooltipContainer = d3.select('.bar-chart .metadata-group');
-tooltipContainer.datum([]).call(tooltip);
+    getNumero('Docente')
+        .then((response) => dataChart.push(['Docentes', response[0].totalvinculo]))
+        .then(() =>
+            getNumero('Servidor')
+                .then((response) => dataChart.push(['Servidores', response[0].totalvinculo]))
+                .then(() => createBarChart(dataChart))
+            )
 
 
 }
 
-function docentes(){
-  fetch(base + 'Docente').then(function(response){
-    response.text().then(function(saida){
-      console.log(saida);
+function createBarChart(dataChart) {
+    c3.generate({
+        bindto: '#numeros',
+        data: {
+            columns: dataChart,
+        	type: 'bar'
+        }
     });
-  });
 }
 
-function servidores(){
-  fetch(base + 'Servidor').then(function(response){
-    response.text().then(function(saida){
-      console.log(saida);
-    });
-  });
+function getNumero(endpoint) {
+    return new Promise(
+        function(resolve, reject) {
+            fetch(base + endpoint).then(function(response){
+              response.json().then((saida) => resolve(saida));
+            });
+        }
+    );
 }
-
-
-
