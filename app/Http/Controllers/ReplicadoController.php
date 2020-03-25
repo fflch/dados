0a862156ac2d0920095c5b7effb5318f563fe570
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-03-24 23:49:15 -0300

    um exemplo que funciona

--- app/Http/Controllers/ReplicadoController.php
+++ app/Http/Controllers/ReplicadoController.php
@@ -3,34 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Khill\Lavacharts\Lavacharts;
+use App\Charts\ExemploChart;
 
 class ReplicadoController extends Controller
 {
     public function exemplo(){
-        $lava = new Lavacharts; // See note below for Laravel
+      $chart = new ExemploChart;
+      $chart->labels(['One', 'Two', 'Three', 'Four']);
+      $chart->dataset('My dataset', 'line', [1, 2, 3, 4]);
+      $chart->dataset('My dataset 2', 'line', [4, 3, 2, 1]);
 
-        $population = $lava->DataTable();
-
-        $population->addDateColumn('Year')
-                   ->addNumberColumn('Number of People')
-                   ->addRow(['2006', 623452])
-                   ->addRow(['2007', 685034])
-                   ->addRow(['2008', 716845])
-                   ->addRow(['2009', 757254])
-                   ->addRow(['2010', 778034])
-                   ->addRow(['2011', 792353])
-                   ->addRow(['2012', 839657])
-                   ->addRow(['2013', 842367])
-                   ->addRow(['2014', 873490]);
-
-        $lava->AreaChart('Population', $population, [
-            'title' => 'Population Growth',
-            'legend' => [
-                'position' => 'in'
-            ]
-        ]);
-
-        return view('exemplo',compact('population'));
+      return view('exemplo', compact('chart'));
     }
 }
--- resources/views/exemplo.blade.php
+++ resources/views/exemplo.blade.php
@@ -1,5 +1,23 @@
-@extends('master')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Chart with VueJS</title>
 
-@section('content')
-@areachart('population', 'pop_div')
-@endsection
+    </head>
+    <body>
+        <div id="app">
+            {!! $chart->container() !!}
+        </div>
+
+        <script src="https://unpkg.com/vue"></script>
+        <script>
+            var app = new Vue({
+                el: '#app',
+            });
+        </script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+        {!! $chart->script() !!}
+    </body>
+</html>
