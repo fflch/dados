4a1985ec040cd8f4b0560189b917ce8bc2652627
Augusto César Freire Santiago <acesarfs@usp.br>
2020-04-06 09:09:00 -0300

    Corrige queries de micros e notes ativos

--- Queries/conta_micros_ativos.sql
+++ Queries/conta_micros_ativos.sql
@@ -1,5 +1,5 @@
 SELECT COUNT(bp.numpat) FROM dbo.BEM b
 INNER JOIN dbo.BEMPATRIMONIADO bp
 ON b.codbem = bp.codbem 
-WHERE (b.coditmmat = 9300 OR b.coditmmat = 45624 OR b.coditmmat = 51110 OR b.coditmmat = 57100 OR b.coditmmat = 61280) 
+WHERE (b.coditmmat = 9300 OR b.coditmmat = 45624 OR b.coditmmat = 51110 OR b.coditmmat = 57100 OR b.coditmmat = 61280  OR b.coditmmat = 354341  OR b.coditmmat = 355020) 
 AND bp.stabem ='Ativo'
