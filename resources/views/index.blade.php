4197d2bda6343bc7826f2258b9be2698ae5964cc
Arthur Ferreira <arthurferreira@usp.br>
2020-04-19 22:27:34 -0300

    Lista autodeclarados ativos da Graduação.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,6 +34,6 @@ Dados do portal:
   <li class="list-group-item"><a href="/concluintesGradPorAno">Série histórica de concluintes da Graduação por ano a partir de 2010.</a></li>
   <li class="list-group-item"><a href="/concluintesPosPorAno">Série histórica de concluintes da Pós-Graduação por ano a partir de 2010.</a></li>
   <li class="list-group-item"><a href="/conveniosAtivos">Totais de convênios ativos.</a></li>
-
+  <li class="list-group-item"><a href="/autodeclaradosGradAtivos">Totais de alunos autodeclarados da Graduação contabilizados por raça/cor.</a></li>
 </ul>
 @endsection
