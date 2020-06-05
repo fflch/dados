11d9ca96e4b09b32bd56d6077c2792ca38176bea
Gabriela Reis <gabrielareisg@hotmail.com>
2020-06-05 16:30:28 -0300

    alterações na consulta docentes ativos por função

--- Queries/conta_docentes_funcao.sql
+++ Queries/conta_docentes_funcao.sql
@@ -1,2 +1,2 @@
 SELECT COUNT (DISTINCT codpes) FROM LOCALIZAPESSOA 
-where tipvinext = 'Docente' and sitatl = 'A' and nomfnc = '__tipo__'
+where tipvinext = 'Docente' and sitatl = 'A' and codundclg = 8 and nomfnc = '__tipo__'
