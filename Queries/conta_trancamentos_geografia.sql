bde35dc4804a866134b2561bd4a49374703c50c5
Arthur Antunes Ferreira <arthurferreira@usp.br>
2020-08-13 01:01:21 -0300

    Total de trancamentos por semestre de todos os cursos da Graduação.

---
+++ Queries/conta_trancamentos_geografia.sql
@@ -0,0 +1,11 @@
+SELECT count (distinct l.codpes)
+FROM LOCALIZAPESSOA l
+    JOIN SITALUNOATIVOGR s
+    ON s.codpes = l.codpes
+    JOIN PESSOA p
+    ON p.codpes = l.codpes
+WHERE l.tipvin = 'ALUNOGR'
+    AND l.codundclg = 8
+    AND s.codcur = 8021
+    AND s.staalu = 'T'
+    AND s.anosem = __semestre__
---
+++ resources/views/trancamentosGeografiaPorSemestre.blade.php
@@ -0,0 +1,12 @@
+@extends('chart')
+
+@section('content_top')
+<a href="/trancamentosGeografiaPorSemestre/export/excel">
+    <i class="fas fa-file-excel"></i> Download Excel</a>
+@endsection
+
+@section('content_footer')
+<center>
+    <p>Total de trancamentos de matrícula por semestre do curso de Geografia. </p>
+</center>
+@endsection
