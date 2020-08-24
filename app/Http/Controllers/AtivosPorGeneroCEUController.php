ea119823c8a0d03606ecb89d1f24dd22b082226a
Arthur Antunes Ferreira <arthurferreira@usp.br>
2020-08-24 08:01:17 -0300

    [Code refactoring]Total de alunos da cultura e extensão por gênero.

--- app/Http/Controllers/AtivosPorGeneroCEUController.php
+++ app/Http/Controllers/AtivosPorGeneroCEUController.php
@@ -13,41 +13,46 @@ class AtivosPorGeneroCEUController extends Controller
     private $data;
     private $excel;
 
-    public function __construct(Excel $excel){
+    public function __construct(Excel $excel)
+    {
         $this->excel = $excel;
         $cache = new Cache();
         $data = [];
 
-        /* Contabiliza alunos de cultura e extensão do gênero feminino */
-        $query = file_get_contents(__DIR__ . '/../../../Queries/conta_alunoceu_fem.sql');
-        $result = $cache->getCached('\Uspdev\Replicado\DB::fetch',$query);
-        $data['Feminino'] = $result['computed'];
+        // Array com os gêneros.  
+        $generos = ['F', 'M'];
 
-        /* Contabiliza alunos de cultura e extensão do gênero masculino */
-        $query = file_get_contents(__DIR__ . '/../../../Queries/conta_alunoceu_masc.sql');
-        $result = $cache->getCached('\Uspdev\Replicado\DB::fetch',$query);
-        $data['Masculino'] = $result['computed'];
+        $query = file_get_contents(__DIR__ . '/../../../Queries/conta_alunoceu_genero.sql');
 
+        /* Contabiliza alunos de cultura e extensão por gênero. */
+        foreach ($generos as $genero) {
+            $query_por_genero = str_replace('__genero__', $genero, $query);
+            $result = $cache->getCached('\Uspdev\Replicado\DB::fetch', $query_por_genero);
+            $data[$genero] = $result['computed'];
+        }
         $this->data = $data;
-    }    
-    
-    public function grafico(){
+    }
+
+    public function grafico()
+    {
         /* Tipos de gráficos:
          * https://www.highcharts.com/docs/chart-and-series-types/chart-types
          */
         $chart = new GenericChart;
-
-        $chart->labels(array_keys($this->data));
+        $chart->labels([
+            'Feminino',
+            'Masculino',
+        ]);
         $chart->dataset('Quantidade alunos por gênero', 'bar', array_values($this->data));
 
         return view('ativosCulturaExtensao', compact('chart'));
     }
 
-    public function export($format){
-        if($format == 'excel') {
-            $export = new DadosExport([$this->data],array_keys($this->data));
+    public function export($format)
+    {
+        if ($format == 'excel') {
+            $export = new DadosExport([$this->data], array_keys($this->data));
             return $this->excel->download($export, 'ativos_cultura_e_extensao.xlsx');
         }
     }
-
 }
