cd1c77b09788d044ef5e039b61f5ee47fd874390
Arthur Antunes Ferreira <arthurferreira@usp.br>
2020-08-23 21:47:10 -0300

    [Code refactoring]Total de estagiários por gênero.

--- app/Http/Controllers/AtivosPorGeneroEstagiariosController.php
+++ app/Http/Controllers/AtivosPorGeneroEstagiariosController.php
@@ -13,40 +13,45 @@ class AtivosPorGeneroEstagiariosController extends Controller
     private $data;
     private $excel;
 
-    public function __construct(Excel $excel){
+    public function __construct(Excel $excel)
+    {
         $this->excel = $excel;
         $cache = new Cache();
         $data = [];
 
-        /* Contabiliza estagiário gênero feminino*/
-        $query = file_get_contents(__DIR__ . '/../../../Queries/conta_estagiario_fem.sql');
-        $result = $cache->getCached('\Uspdev\Replicado\DB::fetch',$query);
-        $data['Feminino'] = $result['computed'];
+        // Array com os gêneros.  
+        $generos = ['F', 'M'];
 
+        $query = file_get_contents(__DIR__ . '/../../../Queries/conta_estagiarios_genero.sql');
 
-        /* Contabiliza estagiário gênero masculino*/
-        $query = file_get_contents(__DIR__ . '/../../../Queries/conta_estagiario_masc.sql');
-        $result = $cache->getCached('\Uspdev\Replicado\DB::fetch',$query);
-        $data['Masculino'] = $result['computed'];
-
+        /* Contabiliza estagiários por gênero. */
+        foreach ($generos as $genero) {
+            $query_por_genero = str_replace('__genero__', $genero, $query);
+            $result = $cache->getCached('\Uspdev\Replicado\DB::fetch', $query_por_genero);
+            $data[$genero] = $result['computed'];
+        }
         $this->data = $data;
-    }    
-    
-    public function grafico(){
+    }
+
+    public function grafico()
+    {
         /* Tipos de gráficos:
          * https://www.highcharts.com/docs/chart-and-series-types/chart-types
          */
         $chart = new GenericChart;
-
-        $chart->labels(array_keys($this->data));
+        $chart->labels([
+            'Feminino',
+            'Masculino',
+        ]);
         $chart->dataset('Gênero', 'bar', array_values($this->data));
 
         return view('ativosEstagiarios', compact('chart'));
     }
 
-    public function export($format){
-        if($format == 'excel') {
-            $export = new DadosExport([$this->data],array_keys($this->data));
+    public function export($format)
+    {
+        if ($format == 'excel') {
+            $export = new DadosExport([$this->data], array_keys($this->data));
             return $this->excel->download($export, 'ativos_estagiarios.xlsx');
         }
     }
