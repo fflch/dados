ec93e931f014c199a00de1290b6065de7ff8d9e8
Ricardo Fernando dos Santos <st.ricardof@gmail.com>
2021-02-04 20:53:19 -0300

    Retirando as informações dos orientandos do replicadosync

--- app/Console/Commands/ReplicadoSyncCommand.php
+++ app/Console/Commands/ReplicadoSyncCommand.php
@@ -66,8 +66,9 @@ class ReplicadoSyncCommand extends Command
                 $info_lattes['jornal_revista'] = Lattes::getTextosJornaisRevistas($docente['codpes'], null, 'anual', -1, null);
                 $info_lattes['trabalhos_anais'] = Lattes::getTrabalhosAnais($docente['codpes'], null, 'anual', -1, null);
                 $info_lattes['outras_producoes_bibliograficas'] = Lattes::getOutrasProducoesBibliograficas($docente['codpes'], null, 'anual', -1, null);
-                $info_lattes['orientandos'] = Posgraduacao::obterOrientandosAtivos($docente['codpes']);
-                $info_lattes['orientandos_concluidos'] = Posgraduacao::obterOrientandosConcluidos($docente['codpes']);
+                
+                //$info_lattes['orientandos'] = Posgraduacao::obterOrientandosAtivos($docente['codpes']);
+                //$info_lattes['orientandos_concluidos'] = Posgraduacao::obterOrientandosConcluidos($docente['codpes']);
                 
                 $lattes->codpes = $docente['codpes'];
                 $lattes->json = $this->safe_json_encode($info_lattes);
