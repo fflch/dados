50cece3f89d5915fc7bc08a290e6707b33356aa0
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-03-22 23:56:56 -0300

    schedule ok

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(ReplicadoSyncCommand::class)->everyThreeHours();
     }
 
     /**
@@ -37,5 +37,6 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+
     }
 }
