5a4c29ad146fb4df3534f040319cd7bba060a3d7
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-03-25 17:11:46 -0300

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\ReplicadoSyncCommand;
 
 class Kernel extends ConsoleKernel
 {
