2f1a633b9d390610336c5db965a75f5652a5520a
Gabriela Reis <gabrielareisg@hotmail.com>
2021-05-05 13:23:46 -0300

    Criação de API que retorna falecidos da FFLCH no período da Covid

--- app/Http/Controllers/Api/PessoaController.php
+++ app/Http/Controllers/Api/PessoaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Pessoa;
+use App\Models\Lattes;
 use App\Http\Requests\PessoaRequest;
 
 class PessoaController extends Controller
@@ -14,17 +15,34 @@ class PessoaController extends Controller
             Pessoa::listar($request->validated())
         );
     }
-    
 
     public function listarFalecidosPorPeriodo(PessoaRequest $request){
+        $falecidos = Pessoa::whereBetween('dtaflc', [$request->dtaini, $request->dtafim])->get()->toArray();
+        
         return response()->json(
-            \Uspdev\Replicado\Pessoa::listarFalecidosPorPeriodo($request->dtaini, $request->dtafim)
+            $falecidos            
         );
     }
 
     public function listarDocentes(){
+        $docentes = Pessoa::where('tipo_vinculo', 'Docente')->get()->toArray();
+        $resultado = [];
+
+        foreach($docentes as $docente){
+            $lattes = Lattes::where('codpes', $docente['codpes'])->first();
+            
+            if($lattes != NULL){
+                $lattes = $lattes->toArray();
+                if(isset($lattes['json']) && $lattes['json'] != NULL && $lattes['json'] != ''){
+                    $json = json_decode($lattes['json']);
+                    $docente['id_lattes'] = $json->id_lattes;
+                }
+            } 
+            array_push($resultado, $docente);
+        }
+        
         return response()->json(
-            \Uspdev\Replicado\Pessoa::listarTodosDocentes()
+            $resultado
         );
     }
 
