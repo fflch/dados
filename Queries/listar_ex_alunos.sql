c99f20ba0a1b1671e89b86c16153dd99badaa36e
Ricardo Fernando dos Santos <st.ricardof@gmail.com>
2021-05-24 20:47:15 -0300

    Ajustando a query listar ex alunos

--- Queries/listar_ex_alunos.sql
+++ Queries/listar_ex_alunos.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT E.codema, P.nompes, T.titpes
 FROM TITULOPES T 
 	INNER JOIN PESSOA P ON T.codpes = P.codpes
-    INNER JOIN EMAILPESSOA E ON T.codpes = E.codpes  
+    LEFT JOIN EMAILPESSOA E ON T.codpes = E.codpes  
     WHERE T.codorg = 8 
     AND T.codcur IN (__curso__)
     AND E.staatnsen = 'S'
