8fcbc24775b2c2a9bb386ab17613dfb9b45b03af
Gabriela Reis <gabrielareisg@hotmail.com>
2021-09-09 20:50:57 -0300

    Código repetido

--- resources/views/ativos.blade.php
+++ resources/views/ativos.blade.php
@@ -1,22 +1,13 @@
 @extends('main')
 
-@section('content_top')
-<a href="/ativos/export/excel">
-    <i class="fas fa-file-excel"></i> Download Excel</a>
-@endsection
-
 @section('content')
 <a href="/ativos/export/excel">
     <i class="fas fa-file-excel"></i> Download Excel</a>
 
 <center>Quantidade de pessoas com vínculos ativos na unidade.</center>
 
-
-
 <div id="chart-div"></div>
 
-
 {!! $lava_col->render('ColumnChart', 'AtivosCOL', 'chart-div') !!}
-
 
 @endsection
