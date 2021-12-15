c3624accb716a13a063663277e14fbbe7ca7a3e7
st-ricardof <st.ricardof@gmail.com>
2021-12-15 18:03:20 -0300

    Adiconando variavel container e solagan e alterando o nome do site

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -13,6 +13,8 @@ $right_menu = [
 
 return [
     'title' => config('app.name'),
+    'slogan' => config('app.slogan'),
+    'container' => 'container container-fflch',
     'skin' => env('USP_THEME_SKIN', 'uspdev'),
     'app_url' => config('app.url'),
     'logout_method' => 'POST',
