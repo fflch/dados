4d261da9c03d86a183a66d4bbb9ba7e37174852a
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2023-07-18 13:22:50 -0300

    concatane version of disciplines

--- resources/views/disciplinas/concatenate.blade.php
+++ resources/views/disciplinas/concatenate.blade.php
@@ -10,13 +10,13 @@
     <a href="{{ config('app.url') }}/turmas">Voltar</a>
 
     @foreach($turmas as $turma)
-        {{ $turma['coddis'] }}{{ $turma['codtur'] }}|: 
-        {{ $turma['coddis'] }} -
+        {{ $turma['coddis'] }}{{ $turma['codtur'] }}|:
+        '{{ $turma['coddis'] }} -
         {{ \App\Utils\ReplicadoTemp::nomdis($turma['coddis']) }} - 
         Turma {{ $turma['codtur'] }}
         ({{ \App\Utils\ReplicadoTemp::horario($turma['coddis'], $turma['codtur']) }}) - 
-        {{ \App\Utils\ReplicadoTemp::ministrantes($turma['coddis'], $turma['codtur']) }}
-        </tr>
+        {{ \App\Utils\ReplicadoTemp::ministrantes($turma['coddis'], $turma['codtur']) }}'
+        <br>
     @endforeach
 
     </div>
