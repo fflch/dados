54b39f8477b3b0670b6205e1b16e3d08857d05dc
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2025-04-09 22:30:22 -0300

    https na produção

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // https na produção
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
